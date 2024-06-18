2b3dd3eac66be988d8ed43500a78cb540a814f84
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-06-18 12:28:16 -0300

    Implements events CRUD (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventsModule } from './events/events.module';
-import { EventsController } from './events/events.controller';
 
 @Module({
   imports: [PrismaModule, EventsModule],
-  controllers: [AppController, EventsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
