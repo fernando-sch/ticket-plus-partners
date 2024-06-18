aa0aa346a421ed47c287b14d73563fd667bc1f6c
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-18 11:18:14 -0300

    feat: add events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { EventsModule } from './events/events.module';
+import { EventsController } from './events/events.controller';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
+  imports: [PrismaModule, EventsModule],
+  controllers: [AppController, EventsController],
   providers: [AppService],
 })
 export class AppModule {}
